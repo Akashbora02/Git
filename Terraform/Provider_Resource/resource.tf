7b49c8c3a56057d1009aa667826c11e6975eb509
Akashbora02 <akashbora0082.com>
2025-11-25 19:57:12 +0530

    tf

---
+++ Terraform/Provider_Resource/resource.tf
@@ -0,0 +1,6 @@
+resource "aws_instance" "demo_tf" {
+    instance_type = "t2.micro"
+   ami = "ami-0fa3fe0fa7920f68e"
+    key_name = "30july"
+   vpc_security_group_ids = [ "sg-0bbfe8e7d4bf3c179" ]
+}
