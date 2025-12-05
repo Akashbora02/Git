479b0a58ca4b1393a0ef94e3f561ab1dae5080a8
Akashbora02 <akashbora0082.com>
2025-12-05 20:35:24 +0530

    tf

--- Terraform/Datablock/main.tf
+++ Terraform/Datablock/main.tf
@@ -6,40 +6,40 @@ resource "aws_instance" "webserver" {
   #  count                   = var.webserver_count
   disable_api_termination = var.webserver_disable_api_termination
 
-  user_data = <<-EOF
+user_data = <<-EOF
               #!/bin/bash
               sudo yum update -y
-              sudo yum install -y nginx
+              sudo yum install -y httpd
+              sudo systemctl enable httpd
+              sudo systemctl start httpd
               cat <<HTML > /var/www/html/index.html
-                <!DOCTYPE html>
-                <html>
-                <head>
-                    <title>Welcome to My EC2 nginx Server</title>
-                    <style>
-                        body {
-                            background-color: #f4f4f9;
-                            font-family: Arial, sans-serif;
-                            text-align: center;
-                            padding: 50px;
-                        }
-                        h1 {
-                            color: #2e86c1;
-                        }
-                        p {
-                            font-size: 18px;
-                            color: #555;
-                        }
-                    </style>
-                </head>
-                <body>
-                    <h1>🚀 Hello from EC2 + nginx!</h1>
-                    <p>This page is served by <b>Nginx</b> running on an Amazon EC2 instance.</p>
-                </body>
-                </html>
-                HTML
-                systemctl enable --now nginx
-                systemctl start nginx
-            EOF
+              <!DOCTYPE html>
+              <html>
+              <head>
+                  <title>Welcome to My EC2 httpd Server</title>
+                  <style>
+                      body {
+                          background-color: #f4f4f9;
+                          font-family: Arial, sans-serif;
+                          text-align: center;
+                          padding: 50px;
+                      }
+                      h1 {
+                          color: #2e86c1;
+                      }
+                      p {
+                          font-size: 18px;
+                          color: #555;
+                      }
+                  </style>
+              </head>
+              <body>
+                  <h1>🚀 Hello from EC2 + Nginx!</h1>
+                  <p>This page is served by <b>Nginx</b> running on an Amazon EC2 instance.</p>
+              </body>
+              </html>
+              HTML
+          EOF
 }
 
 #resource "aws_security_group" "webserver_sg" {
