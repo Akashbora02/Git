bf96c60bfa56e2ae0c1f7956effd029e6e864328
Akashbora02 <akashbora0082.com>
2025-12-05 16:02:12 +0530

    tf

--- Terraform/Datablock/main.tf
+++ Terraform/Datablock/main.tf
@@ -16,17 +16,17 @@ resource "aws_instance" "webserver" {
               EOF
 }
 
-resource "aws_security_group" "webserver_sg" {
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#resource "aws_security_group" "webserver_sg" {
+#  ingress {
+#    from_port   = 0
+#    to_port     = 65535
+#    protocol    = "TCP"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#}
