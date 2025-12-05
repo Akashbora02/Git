04339b233aa48306a706d0228e6d8bb1f50fa3ba
Akashbora02 <akashbora0082.com>
2025-12-05 15:12:23 +0530

    tf

--- Terraform/Datablock/data.tf
+++ Terraform/Datablock/data.tf
@@ -1,16 +1,6 @@
-data "aws_ami" "my_ami" {
-    most_recent = true
-    owners      = ["813592692089"] # Canonical
-      filter {
-        name   = "name"
-        values = ["ami-0fa3fe0fa7920f68e"]
-      }
-      filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-      }
+data "aws_ami" "myami" {
+  name_regex = "ami-0fa3fe0fa7920f68e"
 }
-
 data "aws_security_group" "webserver_my_sg" {
   name = "launch-wizard-4"
 }
