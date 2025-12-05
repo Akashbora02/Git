48332be4a1e80ce2c77f308707a0eec292955f56
Akashbora02 <akashbora0082.com>
2025-12-05 15:26:59 +0530

    tf

--- Terraform/Datablock/data.tf
+++ Terraform/Datablock/data.tf
@@ -1,9 +1,9 @@
 data "aws_ami" "myami" {
     most_recent = true
-    owners      = ["813592692089"] # Canonical
+    owners      = ["amazon"] # Canonical
       filter {
         name   = "name"
-        values = ["al2023-ami-2023.9.20251117.1-kernel-6.1-x86_64"]
+        values = ["al2023-ami-*-x86_64"]
       }
       filter {
         name   = "virtualization-type"
