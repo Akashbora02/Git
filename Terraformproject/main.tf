facff24c219f38f0819f02fb3a30241bf6a06ded
Akashbora02 <akashbora0082.com>
2025-12-07 15:37:54 +0530

    tf

--- Terraformproject/main.tf
+++ Terraformproject/main.tf
@@ -18,33 +18,7 @@ user_data = <<-EOF
               sh docker-install.sh
               cd ..
               docker compose up -d
-              cat <<HTML > /var/www/html/index.html
-              <!DOCTYPE html>
-              <html>
-              <head>
-                  <title>Welcome to My EC2 httpd Server</title>
-                  <style>
-                      body {
-                          background-color: #f4f4f9;
-                          font-family: Arial, sans-serif;
-                          text-align: center;
-                          padding: 50px;
-                      }
-                      h1 {
-                          color: #2e86c1;
-                      }
-                      p {
-                          font-size: 18px;
-                          color: #555;
-                      }
-                  </style>
-              </head>
-              <body>
-                  <h1>🚀 Hello from EC2 + Httpd!</h1>
-                  <p>This page is served by <b>Httpd</b> running on an Amazon EC2 instance.</p>
-              </body>
-              </html>
-              HTML
+
             EOF
 }
 
