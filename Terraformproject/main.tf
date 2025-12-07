94a68a9aa6b3c3b2f480cc1dc93ca7121e9644fd
Akashbora02 <akashbora0082.com>
2025-12-07 18:59:18 +0530

    tf

--- Terraformproject/main.tf
+++ Terraformproject/main.tf
@@ -41,20 +41,7 @@ user_data = <<-EOF
               sh docker-install.sh
               cd ..
               docker compose up -d
-              mysql -h ${aws_db_instance.my_db.address} -u admin -p12345678 -e "CREATE DATABASE studentapp;"
-              
-              mysql -h ${aws_db_instance.my_db.address} -u admin -p12345678 studentapp <<SQL
-              CREATE TABLE if not exists students(student_id INT NOT NULL AUTO_INCREMENT,  
-              student_name VARCHAR(100) NOT NULL,  
-              student_addr VARCHAR(100) NOT NULL,   
-              student_age VARCHAR(3) NOT NULL,      
-              student_qual VARCHAR(20) NOT NULL,     
-              student_percent VARCHAR(10) NOT NULL,   
-              student_year_passed VARCHAR(10) NOT NULL,  
-              PRIMARY KEY (student_id)  
-              );
-            SQL            
-          EOF
+            EOF
 }
 
 
@@ -66,10 +53,3 @@ output "my_db_arn" {
   value = aws_db_instance.my_db.address
 }
 
-resource "local_file" "context_xml" {
-  content = templatefile("${path.module}/context.xml.tpl", {
-    db_endpoint = aws_db_instance.my_db.address
-    db_password = var.aws_db_instance_password
-  })
-  filename = "${path.module}/studentapp/context.xml"
-}
