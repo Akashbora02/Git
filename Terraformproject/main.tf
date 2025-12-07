d9cf1d1319ba3da85156836e34b7ea9340102d28
Akashbora02 <akashbora0082.com>
2025-12-07 13:48:34 +0530

    dev

--- Terraformproject/main.tf
+++ Terraformproject/main.tf
@@ -3,6 +3,20 @@ resource "aws_instance" "webserver" {
   instance_type           = var.webserver_instance_type
   key_name                = var.webserver_key_name
   vpc_security_group_ids  = [var.webserver_vpc_security_group_ids]
-  count                   = var.webserver_count
+ # count                   = var.webserver_count
   disable_api_termination = var.webserver_disable_api_termination
+  user_data = <<-EOF
+  #!/bin/bash
+     sudo apt update -y
+
+     git clone https://github.com/Akashbora02/Git.git
+     cd studentapp/
+
+     chmod 700 docker-install.sh
+     sh docker-install.sh
+
+     docker compose up -d
+  EOF
 }
+
+output ""
--- Terraformproject/variable.tf
+++ Terraformproject/variable.tf
@@ -10,9 +10,9 @@ variable "webserver_key_name" {
 variable "webserver_vpc_security_group_ids" {
   default = "sg-0bbfe8e7d4bf3c179"
 }
-variable "webserver_count" {
-  default = 5
-}
+#variable "webserver_count" {
+#  default = 5
+#}
 variable "webserver_disable_api_termination" {
   default = false
 }
