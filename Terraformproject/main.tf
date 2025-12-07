82d87d4fad3fd00d683cea857598e7c7ed3146f8
Akashbora02 <akashbora0082.com>
2025-12-07 17:33:36 +0530

    tf

--- Terraformproject/main.tf
+++ Terraformproject/main.tf
@@ -1,3 +1,10 @@
+resource "aws_db_subnet_group" "db_subnets" {
+  name       = "rds-subnet-group"
+  subnet_ids = ["subnet-0482a37ccc012331b", "subnet-0ec5c308d2f01e209", "subnet-0aad31b4c64afe074", "subnet-0c64b5c0a24ba3dac", "subnet-047a1f39a17928bfd", "subnet-0b790024640f0ad48"]
+  tags = {
+    Name = "MyDBSubnetGroup"
+  }
+}
 resource "aws_db_instance" "my_db" {
   allocated_storage    = 10
   db_name              = var.aws_db_instance_db_name
@@ -12,13 +19,6 @@ resource "aws_db_instance" "my_db" {
   db_subnet_group_name = aws_db_subnet_group.db_subnets.id
   vpc_security_group_ids = [var.webserver_vpc_security_group_ids]
   skip_final_snapshot  = true
-}
-resource "aws_db_subnet_group" "db_subnets" {
-  name       = "rds-subnet-group"
-  subnet_ids = ["subnet-0482a37ccc012331b", "subnet-0ec5c308d2f01e209", "subnet-0aad31b4c64afe074", "subnet-0c64b5c0a24ba3dac", "subnet-047a1f39a17928bfd", "subnet-0b790024640f0ad48"]
-  tags = {
-    Name = "MyDBSubnetGroup"
-  }
 }
 resource "aws_instance" "webserver" {
   ami                    = var.webserver_ami
@@ -45,6 +45,7 @@ user_data = <<-EOF
               DB_USER = "admin"
               DB_PASS = "${var.aws_db_instance_password}"
               mysql -h $DB_ENDPOINT -u $DB_USER -p$DB_PASS -e "CREATE DATABASE studentapp;"
+
               mysql -h $DB_ENDPOINT -u $DB_USER -p$DB_PASS studentapp <<SQL
               CREATE TABLE if not exists students(student_id INT NOT NULL AUTO_INCREMENT,  
               student_name VARCHAR(100) NOT NULL,  
@@ -55,7 +56,7 @@ user_data = <<-EOF
               student_year_passed VARCHAR(10) NOT NULL,  
               PRIMARY KEY (student_id)  
             );
-            SQL
+            SQL            
             EOF
 }
 
