175c2f8eaf4dc5f43e059aae6c78a54abc805212
Akashbora02 <akashbora0082.com>
2025-12-08 22:47:18 +0530

    tf

--- Terraform/LB_ASG/lb_asg.tf
+++ Terraform/LB_ASG/lb_asg.tf
@@ -1,16 +1,16 @@
-data "aws_vpc" "default_vpc" {
+data "aws_vpc" "default-vpc" {
   default = true
 }
 
-data "aws_subnets" "default_subnets" {
+data "aws_subnets" "default-subnets" {
   filter {
     name   = "vpc_id"
-    values = [data.aws_vpc.default_vpc.id]
+    values = [data.aws_vpc.default-vpc.id]
   }
 }
 
 resource "aws_internet_gateway" "my_IGW" {
-  vpc_id = data.aws_vpc.default_vpc.id
+  vpc_id = data.aws_vpc.default-vpc.id
 
   tags = {
     Name = "my_IGW"
@@ -18,7 +18,7 @@ resource "aws_internet_gateway" "my_IGW" {
 }
 
 resource "aws_route_table" "my_rt" {
-  vpc_id = data.aws_vpc.default_vpc.id
+  vpc_id = data.aws_vpc.default-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -31,7 +31,7 @@ resource "aws_route_table" "my_rt" {
 }
 
 resource "aws_route_table_association" "my_rta" {
-  for_each       = toset(data.aws_subnets.default_subnets.ids)
+  for_each       = toset(data.aws_subnets.default-subnets.ids)
   subnet_id      = each.value
   route_table_id = aws_route_table.my_rt.id
 }
@@ -39,7 +39,7 @@ resource "aws_route_table_association" "my_rta" {
 
 resource "aws_security_group" "my_sg" {
   name   = "my-sg"
-  vpc_id = data.aws_vpc.default_vpc.id
+  vpc_id = data.aws_vpc.default-vpc.id
 
   ingress {
     from_port   = 80
@@ -62,7 +62,7 @@ resource "aws_lb" "my_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.my_sg.id]
-  subnets            = data.aws_subnets.default_subnets.ids
+  subnets            = data.aws_subnets.default-subnets.ids
 }
 
 resource "aws_lb_target_group" "my_lb_tg" {
@@ -135,7 +135,7 @@ resource "aws_autoscaling_group" "asg" {
   desired_capacity          = 2
   min_size                  = 1
   max_size                  = 2
-  vpc_zone_identifier       = data.aws_subnets.default_subnets.ids
+  vpc_zone_identifier       = data.aws_subnets.default-subnets.ids
   target_group_arns         = [aws_route_table.my_rt.arn]
   health_check_grace_period = 120
   health_check_type         = "ELB"
