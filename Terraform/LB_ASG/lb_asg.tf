80ca47da849d7ac53bdd2bb836c0f7203ca9b063
Akashbora02 <akashbora0082.com>
2025-12-09 00:53:39 +0530

    tf

--- Terraform/LB_ASG/lb_asg.tf
+++ Terraform/LB_ASG/lb_asg.tf
@@ -1,26 +1,26 @@
 data "aws_vpc" "default" {
   default = true
 }
-resource "aws_vpc" "main_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+#resource "aws_vpc" "main_vpc" {
+#  cidr_block           = "10.0.0.0/16"
+#  enable_dns_hostnames = true
+#  enable_dns_support   = true
 
-  tags = {
-    Name = "main-vpc"
-  }
-}
+#  tags = {
+#    Name = "main-vpc"
+#  }
+#}
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id            = data.aws_vpc.default.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-  map_public_ip_on_launch = true
+#resource "aws_subnet" "public_subnet" {
+ # vpc_id            = data.aws_vpc.default.id
+ # cidr_block        = "10.0.1.0/24"
+ # availability_zone = "us-east-1a"
+ # map_public_ip_on_launch = true
 
-  tags = {
-    Name = "public-subnet"
-  }
-}
+ # tags = {
+  #  Name = "public-subnet"
+  #}
+#}
 
 data "aws_subnets" "default" {
   filter {
