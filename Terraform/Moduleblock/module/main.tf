a714c1606ea1f7a2b952ed261e22d9009ed642e3
Akashbora02 <akashbora0082.com>
2025-12-10 02:22:07 +0530

    tf

--- Terraform/Moduleblock/module/main.tf
+++ Terraform/Moduleblock/module/main.tf
@@ -5,7 +5,7 @@ module "VPC" {
   public_ip   = true
 }
 module "EC2" {
-  source                            = "/../../../../../../../../../EC2"
+  source                            = "../../../../../../../../../EC2"
   webserver_ami                     = "ami-0ecb62995f68bb549"
   webserver_instance_type           = "t2.micro"
   webserver_key_name                = "30july"
