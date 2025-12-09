d47a9df75435f70b6d98a50b53c918f3ef2b0a4a
Akashbora02 <akashbora0082.com>
2025-12-09 23:50:50 +0530

    tf

--- Terraform/Moduleblock/main/VPC/main.tf
+++ Terraform/Moduleblock/main/VPC/main.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "my_vpc" {
   cidr_block = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 }
 
 resource "aws_subnet" "my_subnetA" {
@@ -26,6 +28,20 @@ route_table_id = aws_route_table.public_rt.id
 }
 resource "aws_security_group" "webserver_sg" {
   vpc_id = aws_vpc.my_vpc.id
+
+ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   ingress {
     from_port   = 0
     to_port     = 65535
