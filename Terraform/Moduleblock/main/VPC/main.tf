7199aef3f869f0136180f3932d9a0ec189387da8
Akashbora02 <akashbora0082.com>
2025-12-09 23:19:21 +0530

    tf

--- Terraform/Moduleblock/main/VPC/main.tf
+++ Terraform/Moduleblock/main/VPC/main.tf
@@ -11,17 +11,12 @@ resource "aws_subnet" "my_subnetA" {
 resource "aws_security_group" "webserver_sg" {
   vpc_id = aws_vpc.my_vpc.id
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 0
+    to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
   egress {
     from_port   = 0
     to_port     = 0
