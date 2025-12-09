2f2d3048328a33e5881f68469bc12863a1abbeaa
Akashbora02 <akashbora0082.com>
2025-12-09 22:46:22 +0530

    tf

--- Terraform/Moduleblock/module/main.tf
+++ Terraform/Moduleblock/module/main.tf
@@ -1,16 +1,16 @@
 module "ec2" {
-    source = "/mnt/c/Users/AB/Desktop/Git/Terraform/Moduleblock/main/EC2"
-    webserver_ami = "ami-0ecb62995f68bb549"
-    webserver_instance_type = "t2.micro"
-    webserver_key_name = "30july"
-    webserver_subnetA = module.vpc.subnet_id
-    webserver_sg = module.vpc.webserver_sg
-    webserver_disable_api_termination = true
+  source                            = "/mnt/c/Users/AB/Desktop/Git/Terraform/Moduleblock/main/EC2"
+  webserver_ami                     = "ami-0ecb62995f68bb549"
+  webserver_instance_type           = "t2.micro"
+  webserver_key_name                = "30july"
+  webserver_subnetA                 = module.vpc.subnet_id
+  webserver_sg                      = module.vpc.webserver_sg
+  webserver_disable_api_termination = true
 }
 
 module "vpc" {
-  source = "/mnt/c/Users/AB/Desktop/Git/Terraform/Moduleblock/main/VPC"
-  vpc_cidr = "10.0.0.0/16"
+  source      = "/mnt/c/Users/AB/Desktop/Git/Terraform/Moduleblock/main/VPC"
+  vpc_cidr    = "10.0.0.0/16"
   vpc_subnetA = "10.0.1.0/24"
-  public_ip = true
+  public_ip   = true
 }
