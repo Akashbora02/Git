07993545348baa009b6060e4b54f89cbda92870f
Akashbora02 <akashbora0082.com>
2025-12-10 15:14:49 +0530

    tf

---
+++ Terraform/tfvars/variable.tf
@@ -0,0 +1,15 @@
+variable "instance_type" {
+#  default = "t2.small"
+}
+
+variable "ami" {
+ # default = "ami-0f00d706c4a80fd93"
+}
+
+variable "key_name" {
+ # default = "30july"
+}
+
+variable "sg" {
+ # default = "sg-0bbfe8e7d4bf3c179"
+}
