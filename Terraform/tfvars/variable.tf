18c1f810eed827ee61c77c23004f57c6ef6dc20e
Akashbora02 <akashbora0082.com>
2025-12-10 15:23:27 +0530

    tf

--- Terraform/tfvars/variable.tf
+++ Terraform/tfvars/variable.tf
@@ -1,15 +1,15 @@
 variable "instance_type" {
-#  default = "t2.small"
+  #  default = "t2.small"
 }
 
 variable "ami" {
- # default = "ami-0f00d706c4a80fd93"
+  # default = "ami-0f00d706c4a80fd93"
 }
 
 variable "key_name" {
- # default = "30july"
+  # default = "30july"
 }
 
 variable "sg" {
- # default = "sg-0bbfe8e7d4bf3c179"
+  # default = "sg-0bbfe8e7d4bf3c179"
 }
