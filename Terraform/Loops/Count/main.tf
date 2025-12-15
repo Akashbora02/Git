0044e2df4892da671592854f0dc96edf5702b42e
Akashbora02 <akashbora0082.com>
2025-12-15 15:56:46 +0530

    tf

--- Terraform/Loops/Count/main.tf
+++ Terraform/Loops/Count/main.tf
@@ -1,8 +1,20 @@
-resource "aws_iam_user" "iam_user" {
+/*resource "aws_iam_user" "iam_user" {
   name = var.user_list[count.index]
   count = length(var.user_list)
 }
 
 output "names" {
  value = [for username in aws_iam_user.iam_user : username.name] 
+} */
+
+
+resource "aws_instance" "my-instance" {
+  instance_type = var.my-instance-inst-type
+  key_name = var.my-instance-key_name
+  ami = var.my-instance-ami_list[count.index]
+  vpc_security_group_ids =  var.my-instance-security_groups
+}
+
+output "my-instance-ami-id" {
+  value = [for ami-id in var.aws_instance.my-instance : ami-id.id]
 }
