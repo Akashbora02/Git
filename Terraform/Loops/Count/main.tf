07d431b35d4802ab7b6ed48d6f13ecc61aa531b3
Akashbora02 <akashbora0082.com>
2025-12-15 15:26:57 +0530

    tf

--- Terraform/Loops/Count/main.tf
+++ Terraform/Loops/Count/main.tf
@@ -1,11 +1,8 @@
 resource "aws_iam_user" "iam_user" {
-  count = length(var.user_list)
   name = var.user_list[count.index]
+  count = length(var.user_list)
 }
 
 output "name" {
- value = [
-    for user_name in var.user_list:
-    aws_iam_user.iam_user[user_name].name
- ] 
+ value = aws_iam_user.iam_user.name 
 }
