ddd1362dd347c5e20877a7ede9b079ae91d72519
Akashbora02 <akashbora0082.com>
2025-12-16 11:18:55 +0530

    tf

--- studentapp-tf-provisioner/main.tf
+++ studentapp-tf-provisioner/main.tf
@@ -2,15 +2,15 @@ data "aws_vpc" "db_vpc" {
   default = true
 }
 resource "aws_db_subnet_group" "db_subnets" {
-  depends_on = [ data.aws_vpc.db_vpc ]
+  depends_on = [data.aws_vpc.db_vpc]
   name       = "rds-subnet-group"
-  subnet_ids = [ data.aws_vpc.db_vpc.id ]
+  subnet_ids = [data.aws_vpc.db_vpc.id]
   tags = {
     Name = "MyDBSubnetGroup"
   }
 }
 resource "aws_db_instance" "my_db" {
-  depends_on = [ aws_db_subnet_group.db_subnets ]
+  depends_on             = [aws_db_subnet_group.db_subnets]
   allocated_storage      = 20
   db_name                = var.aws_db_instance_db_name
   engine                 = var.aws_db_instance_engine
@@ -26,15 +26,15 @@ resource "aws_db_instance" "my_db" {
   skip_final_snapshot    = true
 }
 resource "aws_instance" "webserver" {
-  depends_on = [ aws_db_instance.my_db ]
-  ami                    = var.webserver_ami
-  instance_type          = var.webserver_instance_type
-  key_name               = var.webserver_key_name
-  vpc_security_group_ids = var.webserver_vpc_security_group_id
+  depends_on              = [aws_db_instance.my_db]
+  ami                     = var.webserver_ami
+  instance_type           = var.webserver_instance_type
+  key_name                = var.webserver_key_name
+  vpc_security_group_ids  = var.webserver_vpc_security_group_id
   disable_api_termination = var.webserver_disable_api_termination
 
   provisioner "file" {
-    source = "user-data.sh"
+    source      = "user-data.sh"
     destination = "/home/ubuntu/opt/user-data.sh"
   }
 
@@ -43,16 +43,16 @@ resource "aws_instance" "webserver" {
   }
 
   connection {
-    type = "ssh"
-    user = "ubuntu"
+    type        = "ssh"
+    user        = "ubuntu"
     private_key = file("${path.module}/tf.pem")
-    host = self.public_ip
+    host        = self.public_ip
   }
 
   provisioner "remote-exec" {
-    inline = [ 
-    "chmod 700 user-data.sh",
-    "sh user-data.sh"
+    inline = [
+      "chmod 700 user-data.sh",
+      "sh user-data.sh"
     ]
   }
 }
