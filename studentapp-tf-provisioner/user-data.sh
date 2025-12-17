2c2a00c40a2dd86db2df83e95b6cacc6468bd716
Akashbora02 <akashbora0082.com>
2025-12-17 15:29:42 +0530

    tf

--- studentapp-tf-provisioner/user-data.sh
+++ studentapp-tf-provisioner/user-data.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-
 sudo apt update -y
 sudo apt install -y git mysql-server
 
-sudo apt install nginx -y
+cd /opt/
+
+sudo git clone https://github.com/Akashbora02/Git.git
+cd /opt/Git/studentapp/
+
+chmod 700 docker-install.sh
+sh docker-install.sh
+
+cd ..
+docker compose up -d
